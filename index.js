2f1f04724f60be406442a6608529c3f68ed4d71e
HalimaTusSadiya <halimasaadiya66@gmail.com>
2023-10-20 02:21:37 +0600

    faq objects removed

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-require("dotenv").config();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -9,9 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Server Is Running");
-});
+
 
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.3azmgms.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -24,39 +23,16 @@ const client = new MongoClient(uri, {
   },
 });
 
-const faqs = [
-  {
-    question: "What is your return policy for defective items?",
-    answer:
-      "Contact our customer service within 30 days for a replacement or refund.",
-  },
-  {
-    question: "How do I track my order?",
-    answer:
-      "You'll receive an email with the tracking number. It's also in your account.",
-  },
-  {
-    question: "What payment methods do you accept?",
-    answer: "We accept credit/debit cards, PayPal, and other secure options.",
-  },
-  {
-    question: "Can I change or cancel my order after placing it?",
-    answer:
-      "Orders are processed immediately, so changes or cancellations aren't possible.",
-  },
 
-  {
-    question: "What is the warranty on your products?",
-    answer:
-      "Most products have a 1-year manufacturer's warranty. Check the product listing for details.",
-  },
- 
-];
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    app.get("/", (req, res) => {
+      res.send("Server Is Running");
+    }); 
 
     const productsCollection = client.db("brandShopDB").collection("products");
     const sliderCollection = client.db("brandShopDB").collection("brand-slider");
